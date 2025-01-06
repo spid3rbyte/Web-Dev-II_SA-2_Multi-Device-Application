b29deec30303ae596bcd0df7b60e1d169b8cff43
aysa <aysaakther17@gmail.com>
2025-01-06 14:43:47 +0400

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,11 @@
-// this library is used to interact w a mongodb database
-// helps define structure of docs in a mongodb collection through smthn called schemas
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-// a schema in mongoose defines the structure of the docs that'll be stored in mongodb 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String, //the value must be of type string 
-        required: true //this field is mandatory, doc wont be saved w out it 
-    },
-    password: {
-        type: String,
-        required: true
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 });
 
-export default mongoose.model("User, userSchema");
+const User = mongoose.model('User', userSchema);
+
+export default User;
